7068f67f4008e2591632fc1d69fa40949d2efa19
thejessleigh <j.l.unrein@gmail.com>
2018-11-05 14:29:53 -0600

    add views and js to power them.

---
+++ public/js/modal.js
@@ -0,0 +1,16 @@
+var closeButtons = document.getElementsByClassName("close-button");
+var triggerList = ["noise", "breathe", "confetti", "dev", "mentoring", "zen", "why"];
+
+
+function toggleModal(triggerId) {
+    var element = document.querySelector(triggerId + "modal");
+    element.classList.toggle("show-modal");
+}
+
+Array.from(triggerList).forEach(function(triggerId) {
+   element = document.getElementById(triggerId);
+   element.addEventListener("click", function() {toggleModal("#" + triggerId + "-")})
+});
+Array.from(closeButtons).forEach(function(closeButton){
+    closeButton.addEventListener("click", function() {toggleModal(".show-")})
+});
